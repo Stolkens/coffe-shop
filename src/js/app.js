fff1cc557af89586b260b3aa7b2df1717902f0c2
Stolkens <92957952+Stolkens@users.noreply.github.com>
2023-02-20 02:29:21 +0100

    Add initPage and activatePage

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,58 @@ import Product from './product.js';
 
 
 const app = {
+  initPages: function() {     /* uruchamiana w momencie odswiezenia strony*/
+    const thisApp = this;
+    thisApp.pages = document.querySelector('#pages').children; /* kontenery podstron */
+    
+    thisApp.navLinks = document.querySelectorAll('.navbar a'); /* wszystkie linki prowadzace do  podstron*/
+
+    const idFromHash = window.location.hash.replace('#/', '');      /* z url strony wydobywamy id podstrony ktora ma byc otwarta jako domyslna */
+ 
+    let pageMatchingHash = thisApp.pages[0].id;
+
+    for(let page of thisApp.pages){
+      if(page.id==idFromHash){          /* sprawdzamy czy ktoras z podstron pasuje do idFromHash,  */
+        pageMatchingHash = page.id;     /* jesli nie to wyswietlamy domyslna strone - czyli pierwsza [0] */
+        break;                          /* jesli pasuje to przypisujemy jej id jako domyslna strone */
+      }
+    }
+    thisApp.activatePage(pageMatchingHash);   /* a ktywujemy odpowiednia postrone*/
+
+    for(let navLink of thisApp.navLinks){
+      navLink.addEventListener('click', function(event){    /* dodajemy nasluchiwacze do wszystkich linkow od podstron */
+        event.preventDefault();
+        const clickedElement = this;
+        const id = clickedElement.getAttribute('href').replace('#', ''); /*na klikniecie wydobywamy atrubut href kliknietego linka */
+        thisApp.activatePage(id);       /*i aktywujemy odpowiednia strone o tym id */
+
+        window.location.hash = '#/' + id;     /* zmieniamy url zeby strona nie przeskakiwala do sekcji z dana podstrona */
+      });
+    }
+
+  },
+  activatePage: function(pageId){
+    const thisApp = this;
+    for(let page of thisApp.pages){
+      if (page.id == pageId){
+        page.classList.add('active');
+      }
+      else{
+        page.classList.remove('active');
+      }
+    }
+
+    for(let link of thisApp.navLinks){
+      if (link.getAttribute('href')=='#'+ pageId){
+        link.classList.add('active');
+      }
+      else{
+        link.classList.remove('active');
+      }
+    }
+    
+
+  },
 
   initData: function(){
     const thisApp = this;
@@ -26,6 +78,8 @@ const app = {
   init: function(){
     const thisApp = this;
     thisApp.initData();
+    thisApp.initPages();
+
 
   }
 };
